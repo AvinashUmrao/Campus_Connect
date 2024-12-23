57400b971b43f4d8979ad09c146b85a422dddb51
supraik <137292217+supraik@users.noreply.github.com>
2024-12-23 15:07:59 +0530

    Add files via upload

---
+++ signINsignUP/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+//import SignInSignUp from './SignInSignUp';
+import SignUp  from './SignUp.jsx';
+import './index.css';
+
+const App =()=>{
+  // return <SignInSignUp/>
+  return <SignUp/>
+};
+
+export default App;
