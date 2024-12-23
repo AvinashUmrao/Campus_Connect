86c7a82b2d516aef8989bbcb2191f7d8526a2d23
AvinashUmrao <162718233+AvinashUmrao@users.noreply.github.com>
2024-12-23 13:15:03 +0530

    Delete adminRoutes.js

--- adminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { addEvent, deleteEvent } = require('../controllers/adminController');
-const router = express.Router();
-
-router.post('/add-event', addEvent);
-router.delete('/delete-event/:eventId', deleteEvent);
-
-module.exports = router;
